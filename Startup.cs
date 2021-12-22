ed9153025cbf1ac1bbc5e134b4de8af72f59604f
davseby <SE.David@protonmail.com>
2021-12-22 13:32:36 +0200

    export http to 5001

--- Startup.cs
+++ Startup.cs
@@ -41,11 +41,11 @@ namespace FilmaiOutAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmaiOutAPI v1"));
-
-            app.UseHttpsRedirection();
+	    if(env.IsDevelopment()) {
+		app.UseDeveloperExceptionPage();
+            	app.UseSwagger();
+            	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmaiOutAPI v1"));
+	    }	
 
             app.UseRouting();
 
